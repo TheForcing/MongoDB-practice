7ded9ea25e98d513907c8437a6228228cc856a76
박지훈 <qkrw6@naver.com>
2021-09-06 14:45:47 +0900

    find, findOne

--- mongoshell-basic.js
+++ mongoshell-basic.js
@@ -45,4 +45,69 @@ post
 post.createdAt = new Date();
 // save: 다큐멘트에 _id가 필드가 있으면
 //       update(갱신)
-db.posts.save(post)
+db.posts.save(post)
+// 기존 문서의 갱신(Update)
+/*
+db.컬렉션명.update(
+              {  변경 문서의 조건  } ,
+              {  $set:  
+                        { 업데이트 할 내용}
+              }
+);
+*/
+db.posts.update(
+            { "title" : "First Post"},
+            { $set:
+                      { createdAt: new Date(),
+                        updateAt: new Date() }
+             }
+)
+//객체의 삭제 : .remove
+post = db.posts.findOne()
+db.posts.remove(post)
+             
+//검색 조건 객체를 이용한 삭제
+db.posts.remove({title: /Second/})
+
+/* db.posts 컬랙션에
+title: "First Post", by: "bit", likes:10
+title: "Second Post", by: "hong", likes:30
+title: "Third Post", by: "bit", likes:50
+title: "Fourth Post", by: "hong", likes:10
+
+Insert연습
+*/
+db.posts.insert({
+    title: "First Post",
+    by: "bit",
+    likes:10
+})
+db.posts.insert({
+    title: "Second Post",
+    by: "hong",
+    likes:30
+})
+db.posts.insert({
+    title: "Third Post",
+    by: "bit",
+    likes:50
+})
+db.posts.insert({
+    title: "Fourth Post",
+    by: "hong",
+    likes:10
+})
+//여러러문서를 insert-insertMany
+db.posts.insertMany([
+     {title: "Fifth Post",
+        by: "bit",
+      likes : 50},
+      { title: "Sixth Post",
+         by: "hong",
+        likes: 50}
+])
+//문서의 검색
+//findOne: 조건을 만족하는 문서 중 한개를 반환
+// find(): 조건을 만족하는 문서의 커서를 봔환
+db.posts.findOne()
+db.posts.find()
