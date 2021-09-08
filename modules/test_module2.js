93eb44e4b625063a191617d99323b171ff794332
박지훈 <qkrw6@naver.com>
2021-09-08 10:19:38 +0900

    require

---
+++ modules/test_module2.js
@@ -0,0 +1,12 @@
+const area = {
+    square: (length) =>{
+        return length*length;
+    },
+    circle: radius => {
+        return Math.PI* radius **2;
+    },
+    rectangle: (width, height) =>{
+        return width* height;
+    }
+}
+module.exports = area;
