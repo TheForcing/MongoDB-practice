94660773c67d2dffeec23499b913f98ca1796fb7
박지훈 <qkrw6@naver.com>
2021-09-10 17:45:50 +0900

    api list 1

--- router/APIRouter.js
+++ router/APIRouter.js
@@ -6,7 +6,7 @@ function APIRouter(app) {
     router.get("/friends.json", (req, resp) => {
         let db = app.get("db");
 
-        db.collection("friends").find().toArray()
+        db.collection("friends").find().limit(1).toArray()
         .then(result => {
             //  json 결과 출력
             resp.status(200)
@@ -18,46 +18,7 @@ function APIRouter(app) {
     return router;
 };
 
-//사용자 정보 수정
-router.get("/friends/modify/:id", (req,resp)=>{
-    let id= req.params.id;
-    let db = app.get("db");
-    db.collection("friends").findOne({id: ObjectId(req.params.id)})
-      .then(result=>{
-          resp.render("friends_modify",{friend:result} );
-      }).catch(reason=>{
-          resp.status(500)
-              .send("<p>사용자 정보가 없습니다.</p>");
-      })
 
-});
-
-router.post("/friends/update" , (req,resp)=>{
-    let id= req.body.id;
-    const name = req.body.name;
-    const species = req.body.species;
-    const age = parseInt(req.body.age);
-    let db= app.get("db");
-    db.collection("friends").updateOne(
-             {id:req.params.id},
-           { $set: 
-                {name:req.body.name,  species:species.body.species , age: req.body.age}
-                   
-        }
-    )
-    .then(result=>{
-        console.log(result);
-        resp.status(200);
-         resp.redirect("/web/friends");
-        
-
-    }).catch(reason=>{
-        console.error(reason);
-        resp.status(500);
-          resp.send("업데이트 하지 못하였습니다.");
-    })
-
-});
 
 
 
